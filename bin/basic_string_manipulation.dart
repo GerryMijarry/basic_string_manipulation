bd750d55a43caaecbee93a10ccae7d146ee3621c
gerard <gerardmijares39@gmail.com>
2022-12-08 11:12:35 +0100

    generated project and added forst code from tutorial

---
+++ bin/basic_string_manipulation.dart
@@ -0,0 +1,5 @@
+void main() {
+  const userInput = 'jkashkjSSdha@Uiomsn.com';
+  final lowercase = userInput.toLowerCase();
+  print(lowercase);
+}
---
+++ test/basic_string_manipulation_test.dart
@@ -0,0 +1,8 @@
+import 'package:basic_string_manipulation/basic_string_manipulation.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
